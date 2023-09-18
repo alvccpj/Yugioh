fc4dc51524dac28caa79093acac93938cb2bb821
ViniRocham <viniciusrocha1004@gmail.com>
2023-09-18 19:50:02 +0000

    continuando...

--- src/Loja.java
+++ src/Loja.java
@@ -29,5 +29,29 @@ public class Loja {
 
     //metodos
 
+    public void compraDeBooster(Usuario usuario) {
+        
+        int precoBooster = 10; //pode ser qualquer preco aqui
+
+        if (usuario.getCardCoinsUsuario() >= precoBooster) {
+            
+            usuario.setCardCoins(usuario.getCardCoinsUsuario() - precoBooster); 
+
+            Carta[] inventario = usuario.getInventario();
+            for (int i = 0; i < 12; i++) {
+                
+                Carta cartaAleatoria = gerarCartaAleatoria();
+                adicionarCartaAoInventario(inventario, cartaAleatoria);
+            }
+
+                System.out.println("BOOSTER COMPRADO COM SUCESSO!!");
+        } 
+        
+            else {
+                System.out.println("VOCÊ NÃO TEM CARDCOINS SUFICIENTES PARA COMPRAR O BOOSTER!");
+        }
+    }
+
+    
 
 }
