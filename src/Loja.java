e5df35b73096393c46732b8a7154a86b57f4715d
Ribauu <vinicius.2021104974@unicap.br>
2023-09-18 18:27:05 -0300

    add_loja

--- src/Loja.java
+++ src/Loja.java
@@ -1,21 +1,19 @@
+import java.util.Random;
+
 public class Loja {
     private String numeroCartao;
     private String codigoVerificador;
 
-    //constructor
-
-    public Loja(String numeroCartao, String codigoVerificador){
+    public Loja(String numeroCartao, String codigoVerificador) {
         this.numeroCartao = numeroCartao;
         this.codigoVerificador = codigoVerificador;
     }
 
-    //get e set
-
-    public String getNumcartao() {
+    public String getNumeroCartao() {
         return numeroCartao;
     }
 
-    public void setNumcartao(String numeroCartao) {
+    public void setNumeroCartao(String numeroCartao) {
         this.numeroCartao = numeroCartao;
     }
 
@@ -27,31 +25,51 @@ public class Loja {
         this.codigoVerificador = codigoVerificador;
     }
 
-    //metodos
+    public void compraDeBooster(Usuario usuario, Inventario Inventario) {
+        int precoBooster = 10; // Preço do booster em cardcoins (ajuste conforme necessário)
 
-    public void compraDeBooster(Usuario usuario) {
-        
-        int precoBooster = 10; //pode ser qualquer preco aqui
+        if (usuario.getCardcoinsUsuario() >= precoBooster) {
+            usuario.setCardcoinsUsuario(usuario.getCardcoinsUsuario() - precoBooster);
 
-        if (usuario.getCardCoinsUsuario() >= precoBooster) {
-            
-            usuario.setCardCoins(usuario.getCardCoinsUsuario() - precoBooster); 
+            Inventario inventario = usuario.getInventario();
+            Carta[] boosterPack = gerarCartaAleatoria();
 
-            Carta[] inventario = usuario.getInventario();
-            for (int i = 0; i < 12; i++) {
-                
-                Carta cartaAleatoria = gerarCartaAleatoria();
-                adicionarCartaAoInventario(inventario, cartaAleatoria);
+            for (Carta cartaAleatoria : boosterPack) {
+                adicionarCartaAoInventario(inventario, cartaAleatoria, usuario, inventario);
             }
 
-                System.out.println("BOOSTER COMPRADO COM SUCESSO!!");
-        } 
-        
-            else {
-                System.out.println("VOCÊ NÃO TEM CARDCOINS SUFICIENTES PARA COMPRAR O BOOSTER!");
+            System.out.println("BOOSTER COMPRADO COM SUCESSO!!");
+        } else {
+            System.out.println("VOCÊ NÃO TEM CARDCOINS SUFICIENTES PARA COMPRAR O BOOSTER!");
         }
     }
 
-    
+    public Carta[] gerarCartaAleatoria(Carta carta) {
+        int qtdCardsBoosterPack = 12;
+        Carta[] boosterPack = new Carta[qtdCardsBoosterPack];
+
+        for (int posBoosterPack = 0; posBoosterPack < qtdCardsBoosterPack; posBoosterPack++) {
+            boosterPack[posBoosterPack] = carta;
+            
+        }
+        return boosterPack;
+    }
+    public void adicionarCartaAoInventario(Inventario inventario, Carta carta, Usuario usuario) {
+    int limiteCartasRepetidas = 3; // Limite de cartas repetidas permitidas
+
+    // Verificar se o usuário já possui o limite de cartas repetidas da mesma carta
+    int quantidadeCartaNoInventario = usuario.getInventario().contarCartasIguais(carta);
+    if (quantidadeCartaNoInventario < limiteCartasRepetidas) {
+        // Adicionar a carta ao inventário
+        inventario.adicionarCartaAoInventario(carta);
+
+        System.out.println("Carta Adicionada ao Inventario: " + carta.getNomeCarta());
+    } else {
+        // O usuário já possui o limite de cartas repetidas, ganhar Cardacoins
+        int valorCartasRepetidas = calcularValorCartasRepetidas(carta, quantidadeCartaNoInventario);
+        usuario.setCardcoinsUsuario(usuario.getCardcoinsUsuario() + valorCartasRepetidas);
+        System.out.println("Você já possui o limite de cartas repetidas. Ganhou " + valorCartasRepetidas + " Cardacoins.");
+    }
+}
 
 }
