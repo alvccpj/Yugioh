296bfe08bfa6f176212f079cfa4908bfba1ab353
ViniRocham <viniciusrocha1004@gmail.com>
2023-09-18 18:47:29 +0000

    constructor and get e set (loja)

--- src/Loja.java
+++ src/Loja.java
@@ -1,4 +1,33 @@
 public class Loja {
     private String numeroCartao;
+    private String codigoVerificador;
+
+    //constructor
+
+    public Loja(String numeroCartao, String codigoVerificador){
+        this.numeroCartao = numeroCartao;
+        this.codigoVerificador = codigoVerificador;
+    }
+
+    //get e set
+
+    public String getNumcartao() {
+        return numeroCartao;
+    }
+
+    public void setNumcartao(String numeroCartao) {
+        this.numeroCartao = numeroCartao;
+    }
+
+    public String getCodigoVerificador() {
+        return codigoVerificador;
+    }
+
+    public void setCodigoVerificador(String codigoVerificador) {
+        this.codigoVerificador = codigoVerificador;
+    }
+
+    //metodos
+
 
 }
