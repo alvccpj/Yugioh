595bd0a2857551e6b57c0a102705a87b258eeb2b
Ribauu <vinicius.2021104974@unicap.br>
2023-10-08 15:49:24 -0300

    mao, mana maxima e saque (falta revisar saque)

--- src/Arena.java
+++ src/Arena.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Arena {
     private Usuario jogador1;
     private Usuario jogador2;
@@ -6,6 +9,11 @@ public class Arena {
     private int vidaJogador1;
     private int vidaJogador2;
     private boolean jogoEncerrado;
+    
+    private int manaMaximaJogador1;
+    private int manaMaximaJogador2;
+    private ArrayList<Carta> cemiterioJogador1;
+    private ArrayList<Carta> cemiterioJogador2;
 
     public Arena(Usuario jogador1, Usuario jogador2, String modalidadeJogo) {
         this.jogador1 = jogador1;
@@ -15,6 +23,12 @@ public class Arena {
         this.vidaJogador1 = 20;
         this.vidaJogador2 = 20;
         this.jogoEncerrado = false;
+        this.campoJogador1 = new Carta[10];
+        this.campoJogador2 = new Carta[10];
+        this.manaMaximaJogador1 = 0;
+        this.manaMaximaJogador2 = 0;
+        this.cemiterioJogador1 = new ArrayList<>();
+        this.cemiterioJogador2 = new ArrayList<>();
     }
 
     public void regraJogo() {
@@ -40,5 +54,45 @@ public class Arena {
         while (!jogoEncerrado) {
             regraJogo();
         }
+    }
+
+    public void saque(Usuario jogador) {
+        Random random = new Random();
+        Carta[] deck = jogador.getDeck();
+        ArrayList<Carta> cartasAleatorias = new ArrayList<>();
+
+        for (int i = 0; i < 7; i++) {
+            int indiceCarta = random.nextInt(deck.length);
+            Carta carta = deck[indiceCarta];
+
+            if (carta != null) {
+                cartasAleatorias.add(carta);
+                deck[indiceCarta] = null;
+            }
+        }
+
+        jogador.setDeck(deck);
+        jogador.setMao(cartasAleatorias.toArray(new Carta[0]));
+    }
+
+    public void retornarCartasAoDeck(Usuario jogador, Carta[] cartas) {
+        Carta[] mao = jogador.getMao();
+        ArrayList<Carta> deck = new ArrayList<>();
+
+        for (Carta carta : cartas) {
+            if (carta != null) {
+                deck.add(carta);
+            }
+        }
+
+        jogador.setMao(new Carta[10]);
+
+        for (int i = 0; i < mao.length; i++) {
+            if (i < deck.size()) {
+                mao[i] = deck.get(i);
+            }
+        }
+
+        jogador.setDeck(mao);
     }
 }
