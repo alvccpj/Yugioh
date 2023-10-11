8d9aef473d4b3f0972aa870fe123da72709fa4bf
Ribauu <vinicius.2021104974@unicap.br>
2023-10-11 15:14:39 -0300

    arena so falta atk

---
+++ src/Mana.java
@@ -0,0 +1,5 @@
+public class Mana extends Carta {
+    public Mana(String nome, String imagem, String raridade, int custo, int mana) {
+        super(nome, imagem, "Mana", raridade, 0, 0, custo, "Geração de mana", 1, mana);
+    }
+}
